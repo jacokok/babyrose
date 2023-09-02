bab23a572c6846da102e27201741326fc425cc14
jacokok <kokjaco2@gmail.com>
2023-09-02 21:08:13 +0000

    Deploying to gh-pages from @ jacokok/babyrose@f56c2564de8d1d5ac8d5e9edc9007b136425eaa6 🚀

---
+++ _app/immutable/chunks/paths.bc17c681.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_146b0oq)==null?void 0:s.base)??"/babyrose";var a;const b=((a=globalThis.__sveltekit_146b0oq)==null?void 0:a.assets)??e;export{b as a,e as b};
